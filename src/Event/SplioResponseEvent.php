89f06f39574ca796515f6993db42bc19fea6296f
Miguel Arber Magjistari <miguel.arber@gmail.com>
2019-11-22 14:01:20 +0100

    SplioResponseEvent updated. Now a method allows the user to silence the exception thrown in case a sync with Splio could not be completed. Optionally, the event may now contain the entity used to generate the JSON entity structure sent to Splio.

--- src/Event/SplioResponseEvent.php
+++ src/Event/SplioResponseEvent.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\splio\Event;
 
+use Drupal\Core\Entity\EntityInterface;
 use Symfony\Component\EventDispatcher\Event;
 
 /**
@@ -15,6 +16,12 @@ use Symfony\Component\EventDispatcher\Event;
  *   Splio's received response. May be a response or an exception.
  * @property array entityStructure
  *   Splio's sent entity structure.
+ * @property \Drupal\Core\Entity\EntityInterface entity
+ *   Entity used to generate the Splio entity structure.
+ * @property bool isSilentException
+ *   In case an exception occurred while trying to sync with Splio, and this
+ *   param is set to TRUE, the exception will not be thrown. No traces in the
+ *   log will appear either. By default, this param is set to FALSE.
  */
 class SplioResponseEvent extends Event {
 
@@ -27,16 +34,22 @@ class SplioResponseEvent extends Event {
    *   Receives the response received from Splio.
    * @param array $entityStructure
    *   Receives the sent Splio entity structure.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   Receives the entity used to generate the Splio entity structure.
    */
-  public function __construct($response, array $entityStructure = NULL) {
+  public function __construct($response, array $entityStructure = NULL, EntityInterface $entity = NULL) {
     $this->response = $response;
     $this->entityStructure = $entityStructure;
+    $this->entity = $entity;
+
+    // By default, if an exception is received, it will be thrown.
+    $this->isSilentException = FALSE;
   }
 
   /**
    * Returns the response received from the Splio API.
    *
-   * @return \Psr\Http\Message\ResponseInterface
+   * @return mixed
    *   Returns the result of the HTTP request made to Splio's API.
    */
   public function getSplioResponse() {
@@ -51,6 +64,37 @@ class SplioResponseEvent extends Event {
    */
   public function getSplioEntityStructure() {
     return $this->entityStructure;
+  }
+
+  /**
+   * Returns the Drupal entity used to generate the structure sent to Splio API.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface
+   *   The Drupal entity used to generate the structure sent to Splio API.
+   */
+  public function getSplioEntity() {
+    return $this->entity;
+  }
+
+  /**
+   * Returns the property isSilentException.
+   *
+   * @return bool
+   *   Returns a bool defining if an exception should be thrown after the event
+   *   dispatch.
+   */
+  public function isSilentException() {
+    return $this->isSilentException;
+  }
+
+  /**
+   * Sets the property isSilentException.
+   *
+   * @param bool $isSilentException
+   *   Defines if an exception should be thrown after the event dispatch.
+   */
+  public function setIsSilentException(bool $isSilentException) {
+    $this->isSilentException = $isSilentException;
   }
 
 }
